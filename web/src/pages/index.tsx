56f3a7af5420b5ae2338b80a5b7dad70f715a632
Lucas Coronel <lccoronel7@icloud.com>
2022-04-28 08:05:52 -0300

    ♻️ auth page

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,45 +1,26 @@
+import { getSession } from "@auth0/nextjs-auth0"
 import { GetServerSideProps } from "next"
-import { getAccessToken, getSession, useUser } from "@auth0/nextjs-auth0"
-import Link from "next/link"
 
 export default function Home() {
-  const {user} = useUser()
-
-  return (
-    <div>
-      <h1>Hello world</h1>
-      <pre>
-        {JSON.stringify(user, null, 2)}
-      </pre>
-      <Link href="/api/auth/login">login</Link>
-      <Link href="/api/auth/logout"> sair</Link>
-    </div>
-  )
+  return null;
 }
 
-export const getServerSideProps: GetServerSideProps = async ({req, res}) => {
-  const session = getSession(req, res)
-  const token = await getAccessToken(req, res)
-  console.log(token);
-  
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  const session = getSession(req, res);
 
   if (!session) {
     return {
       redirect: {
         destination: '/api/auth/login',
-        permanent: false
+        permanent: false,
       }
     }
   } else {
     return {
       redirect: {
         destination: '/app',
-        permanent: false
+        permanent: false,
       }
     }
-  }
-
-  return {
-    props: {}
   }
 }
