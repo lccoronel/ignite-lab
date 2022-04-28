64db2ffa24c42696b4fe4666a98bcd6a1376278b
Lucas Coronel <lccoronel7@icloud.com>
2022-04-28 08:07:11 -0300

    ✨ purchase kafka message

--- purchases/src/services/purchases.service.ts
+++ purchases/src/services/purchases.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 
 import { PrismaService } from 'src/database/prisma/prisma.service';
+import { KafkaService } from 'src/message/kafka.service';
 
 interface CreatePurchaseParams {
   customerId: string;
@@ -9,7 +10,7 @@ interface CreatePurchaseParams {
 
 @Injectable()
 export class PurchasesService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService, private kafka: KafkaService) {}
 
   listAllPurchases() {
     return this.prisma.purchase.findMany({ orderBy: { createdAt: 'desc' } });
@@ -29,8 +30,23 @@ export class PurchasesService {
 
     if (!product) throw new Error('Product not found');
 
-    return await this.prisma.purchase.create({
+    const purchase = await this.prisma.purchase.create({
       data: { customerId, productId },
     });
+
+    const custumer = await this.prisma.customer.findUnique({
+      where: { id: customerId },
+    });
+
+    this.kafka.emit('purchases.new-purchase', {
+      custumer: { authUserId: custumer.authUserId },
+      product: {
+        id: product.id,
+        title: product.title,
+        slug: product.slug,
+      },
+    });
+
+    return purchase;
   }
 }
